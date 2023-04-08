18064a885c5c8a7e577da8231d847df1b1c04f3d
SunilKumarDubey1306 <sunildubeym1306@gmail.com>
2023-04-08 19:31:25 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
-from setuptools import find_packages ,setup
+from setuptools import find_packages,setup
 from typing import List
 
-HIGH_E_DOT = '-e .'
-def get_requirements(file_path:str)-> List(str):
-    """
-    This function will reurn the list of requirements
-    """
-    requirements = []
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        [req.replace ("\n", " ") for req in requirements]
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-        if HIGH_E_DOT in requirements:
-            requirements.remove(HIGH_E_DOT)
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
     
     return requirements
 
-
 setup(
-name = 'MLProjects',
-version = '0.0.1',
-author = 'Sunil',
-author_email = 'sunildubeym1306@gmail.com',
-packages = find_packages(),
-install_requires = get_requirements('requirements.txt')
-
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
 
 )
